25ce1d225dc5cf69b0a209c0a55ca37202a5118a
José Arteaga <zearteaga@gmail.com>
2024-05-13 19:17:48 +0100

    Organized file structure with views and controller. Created files in respect to hourly mode. Simple nav between homescreen and hourly is now possible

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 from os import environ
 environ['DISPLAY'] = ':0' #set to local display
-
+ 
 from kivy.app import App
 from kivy.app import Builder
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
-
-kv = Builder.load_file("homescreen.kv")
-
-LabelBase.register(name="Aptos",
-                   fn_regular="assets/fonts/aptos/aptos.ttf",
-                   fn_bold="assets/fonts/aptos/aptos-bold.ttf",
-                   fn_italic="assets/fonts/aptos/aptos-italic.ttf"
-                   )                                     
+from kivy.uix.screenmanager import ScreenManager
+from controllers.homescreen import Screen_Home
+from controllers.mode_hourly import Screen_Hourly                            
 
 class SolarClock(App):
 
     def build(self):
         Window.maximize()
         Window.borderless = True
-        print(Window.size)
-        return kv
+
+        Builder.load_file("views/common.kv") 
+        Builder.load_file("views/homescreen.kv") 
+        Builder.load_file("views/mode_hourly.kv")
+
+        LabelBase.register(name="Aptos",
+                   fn_regular="assets/fonts/aptos/aptos.ttf",
+                   fn_bold="assets/fonts/aptos/aptos-bold.ttf",
+                   fn_italic="assets/fonts/aptos/aptos-italic.ttf"
+                   )           
+        
+        sm = ScreenManager()
+        sm.add_widget(Screen_Home(name='homescreen'))
+        sm.add_widget(Screen_Hourly(name='hourly'))
+        
+        return sm
 
 if __name__ == '__main__':
     SolarClock().run()
